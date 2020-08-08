976ef4342a85234a161d3dbf16fc7b6c2858f241
Charles Young <cy@unitive.tech>
2020-08-08 16:59:30 +0800

    Convert model evaluator from a code block in a notebook to a python class in a seperate file for better project structure.

--- model_estimation.py
+++ model_estimation.py
@@ -1,29 +1,44 @@
+import logging
+
 from SEM import SemModel
 
 
-class ModelEvaluation:
+class ModelEvaluator:
 
     def __init__(self):
         self.sem = SemModel()
 
-    def evaluate(self, model):
+    def _evaluate_with_SEM(self, model):
         try:
-            buildRes = sem.build_sem_model(model)
-            if not buildRes:
-                return 0
+            if_built = self.sem.build_sem_model(model)
+            if not if_built:
+                return False, _
 
-            fitRes = sem.fit_sem_model(data)
-            if not fitRes['is_fitted']:
-                return 0
+            if_fit = self.sem.fit_sem_model(data)
+            if not if_fit['is_fitted']:
+                return False, _
 
-            measureRes = sem.evaluate_sem_model()
-            if not measureRes['is_evaluated']:
-                return 0
+            measure_indexes = self.sem.evaluate_sem_model()
+            if not measure_indexes['is_evaluated']:
+                return False, _
 
-            AGFI = measureRes['agfi']
-            RMSEA = measureRes['rmsea']
-            index = AGFI - RMSEA * 10
-            sigmoidIndex = 1 / (1 + np.exp(-index))
-            return sigmoidIndex
-        except:
+            return True, measure_indexes
+        except Exception:
+            logging.error(Exception)
+
+            return False, _
+
+    @staticmethod
+    def evaluate(model):
+        if_evaluation_success, indexes = _evaluate_with_SEM(model)
+
+        if not if_evaluation_success:
             return 0
+
+        AGFI = indexes['agfi']
+        RMSEA = indexes['rmsea']
+
+        index = AGFI - RMSEA * 10
+        index = 1 / (1 + np.exp(-index))
+
+        return index
